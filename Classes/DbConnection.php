52afb2850ac51a9239ad9c3bf204b131181f9576
Fatimezzahra <fatimezzahradarim@gmail.com>
2022-12-02 11:14:00 +0100

    Create function connection database

--- Classes/DbConnection.php
+++ Classes/DbConnection.php
@@ -2,5 +2,30 @@
 
 class DbConnection
 {
-
+    public static function connect()
+    {
+        $servername = "localhost";
+        $username = "root";
+        $password = " ";
+        $conn = null;
+        try {
+            $conn = new PDO("mysql:host=$servername;cliniquedb=myDB", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+            //setAttribute() method sets a new value to an attribute.
+            return $conn;
+        } catch (PDOException $e) {
+            return $conn;
+        }
+    }
 }
+/* PDO::ATTR_ERRMODE
+Error reporting mode of PDO. Can take one of the following values:
+
+-->PDO::ERRMODE_SILENT
+Only sets error codes.
+-->PDO::ERRMODE_WARNING
+Raises E_WARNING diagnostics.
+-->PDO::ERRMODE_EXCEPTION
+Throws PDOExceptions. 
+*/
