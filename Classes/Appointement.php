b1b8ed233231388c0294a06d003cfe19a62de53c
Ayoub Ouabi <ayoub.ouabi06@gmail.com>
2022-12-05 09:55:01 +0100

    add proprities and functions into classes

--- Classes/Appointement.php
+++ Classes/Appointement.php
@@ -2,5 +2,79 @@
 
 class Appointement
 {
+    private $appId;
+    private $sessionId;
+    private $date;
+    private $patientId;
 
+    /**
+     * @return mixed
+     */
+    public function getAppId()
+    {
+        return $this->appId;
+    }
+
+    /**
+     * @param mixed $appId
+     */
+    public function setAppId($appId): void
+    {
+        $this->appId = $appId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSessionId()
+    {
+        return $this->sessionId;
+    }
+
+    /**
+     * @param mixed $sessionId
+     */
+    public function setSessionId($sessionId): void
+    {
+        $this->sessionId = $sessionId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate($date): void
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPatientId()
+    {
+        return $this->patientId;
+    }
+
+    /**
+     * @param mixed $patientId
+     */
+    public function setPatientId($patientId): void
+    {
+        $this->patientId = $patientId;
+    }
+
+    function canculAppointement(){
+
+    }
+    function getlAppointement(){
+
+    }
 }
--- Classes/Session.php
+++ Classes/Session.php
@@ -2,5 +2,133 @@
 
 class Session
 {
+    private $sessionId;
+    private $doctorId;
+    private $title;
+    private $sessDate;
+    private $maxNumber;
+
+    /**
+     * @param $sessionId
+     * @param $doctorId
+     * @param $sessDate
+     * @param $maxNumber
+     */
+    public function __construct($sessionId, $doctorId, $sessDate, $maxNumber)
+    {
+        $this->sessionId = $sessionId;
+        $this->doctorId = $doctorId;
+        $this->sessDate = $sessDate;
+        $this->maxNumber = $maxNumber;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSessionId()
+    {
+        return $this->sessionId;
+    }
+
+    /**
+     * @param mixed $sessionId
+     */
+    public function setSessionId($sessionId): void
+    {
+        $this->sessionId = $sessionId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDoctorId()
+    {
+        return $this->doctorId;
+    }
+
+    /**
+     * @param mixed $doctorId
+     */
+    public function setDoctorId($doctorId): void
+    {
+        $this->doctorId = $doctorId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSessDate()
+    {
+        return $this->sessDate;
+    }
+
+    /**
+     * @param mixed $sessDate
+     */
+    public function setSessDate($sessDate): void
+    {
+        $this->sessDate = $sessDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMaxNumber()
+    {
+        return $this->maxNumber;
+    }
+
+    /**
+     * @param mixed $maxNumber
+     */
+    public function setMaxNumber($maxNumber): void
+    {
+        $this->maxNumber = $maxNumber;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+
+    public function cancelSession(){
+
+    }
+
+    public function addSession(){
+
+    }
+    public function removeSesssion(){
+
+    }
+    public function updateSession(){
+
+    }
+
+    public function selectSession(){
+
+    }
+
+    public function BookingSession(){
+
+    }
+
+
+
+
+
+
+
 
 }
