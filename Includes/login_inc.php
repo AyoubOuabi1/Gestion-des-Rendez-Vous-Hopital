7c5a338d53725c1613172ee9c31d159fee61e0d9
Ayoub Ouabi <ayoub.ouabi06@gmail.com>
2022-12-15 10:53:04 +0100

    login

--- Includes/login_inc.php
+++ Includes/login_inc.php
@@ -1,28 +1,19 @@
 <?php
-include '../Classes/login_classes.php' ;
-spl_autoload_register(function($className) {
-    $file = '../Classes'.$className.'.php';
-    if (file_exists($file)) {
-        require $file;
-    }
-});
- if (isset($_POST["submit"]))
+require '../Classes/User.php';
+if (isset($_POST["submit"]))
 {     //Grapping the data
     $email = $_POST["fLoginEmail"];
     $password = $_POST["fLoginPsd"];
-  
-    if(login_classes::getUser($email, $password)==1){
-        echo '</br> admin';
-        // header("location:dashboard.php");
-    } else if (login_classes::getUser($email, $password) == 0) {
-        // echo '</br> user';
-        header('location: ../dashboard/patient/dashboard.php') ;
-        
+    if(User::getUser($email, $password)==0){
+        header("location : http://localhost/Gestion-des-Rendez-Vous-Hopital/Dashboard/admin/Dashboard.php");
 
-    } else if (login_classes::getUser($email, $password) == 2) {
-        echo '</br> doctor';
+        // header("location:dashboard.php");
+    } else if (User::getUser($email, $password) == 1) {
+        header("location : http://localhost/Gestion-des-Rendez-Vous-Hopital/Dashboard/patient/Dashboard.php");
+    } else if (User::getUser($email, $password) == 2) {
+        header("location : http://localhost/Gestion-des-Rendez-Vous-Hopital/Dashboard/doctor/Dashboard.php");
     }else{
         echo"Rien";
     }
-    
+
 }
